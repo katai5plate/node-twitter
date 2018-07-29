f7e24543eaac44c46bae52b35ab560aee92fc4e3
katai5plate <katai5plate.sub@gmail.com>
2018-07-29 09:00:49 +0900

    update

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # node-twitter
+TwitterAPIを使って自分のふぁぼを収集します。
+## つかいかた
+1. おとす
+```s
+git clone https://github.com/katai5plate/node-twitter
+```
+2. いれる
+```coffee
+cd node-twitter
+yarn install
+```
+3. いじる
+- src/auth.js
+```diff
+const Twitter = require('twitter');
+const config = (auth => {
+  let data;
+  try {
+    data = require('../twitter.config.json');
+  } catch (e) {
+    data = auth;
+  }
+  return data;
+})(
+  // keyとtokenを入力
+  {
++   consumer_key: "ここに Consumer API key",
++   consumer_secret: "ここに Consumer API secret key",
++   access_token_key: "ここに Access token",
++   access_token_secret: "ここに Access token secret"
+  }
+);
+const client = new Twitter(config);
+module.exports = client;
+```
+4. 実行
+```coffee
+yarn start <取得ツイート数> <取得開始するツイートID>
+```
--- src/index.js
+++ src/index.js
@@ -1,45 +1,58 @@
+const fs = require("fs");
 const client = require("./auth");
 
-/**
- * @param {number} user_id ユーザーID
- * @param {number} count 取得ツイート数
- * @param {number} [max_id=NaN] どのツイートIDから取得するか
- */
-const setParams = (user_id, count, max_id = Number.NaN) => {
-    let ret = {
-        user_id,
-        count,
-        include_entities: true, // entities取得用
-        tweet_mode: "extended", // 全文表示
-    };
-    if (!isNaN(max_id)) {
-        ret["max_id"] = max_id;
-    }
-    return ret;
-}
-client.get(
-    'favorites/list',
-    setParams("katai5plate", 5),
-    (error, list) => {
-        if (!error) {
-            const result = list.map(tweet => {
-                const { user } = tweet;
-                return {
-                    tweet: {
-                        id: tweet.id,
-                        text: tweet.full_text,
-                        media: tweet.entities.media,
-                    },
-                    user: {
-                        id: user.screen_name,
-                        name: user.name,
-                        image: user.profile_image_url_https,
-                    },
-                };
-            });
-            console.log(result);
-        } else {
-            console.log({ error });
+const _count = process.argv[2] || 5;
+const _fromId = process.argv[3] || Number.NaN;
+
+(() => {
+    let fname = "newest";
+    let fcount = 0;
+
+    /**
+     * @param {number} count 取得ツイート数
+     * @param {number} [max_id=NaN] どのツイートIDから取得するか
+     */
+    const setParams = (count, max_id = Number.NaN) => {
+        let ret = {
+            count,
+            include_entities: true, // entities取得用
+            tweet_mode: "extended", // 全文表示
+        };
+        fcount = `${count}`;
+        if (!isNaN(max_id)) {
+            ret["max_id"] = max_id;
+            fname = `${max_id}`
         }
+        return ret;
     }
-);
+    client.get(
+        'favorites/list',
+        setParams(_count, _fromId),
+        (error, list) => {
+            if (!error) {
+                const result = list.map(tweet => {
+                    const { user } = tweet;
+                    return {
+                        tweet: {
+                            id: tweet.id,
+                            text: tweet.full_text,
+                            media: tweet.entities.media,
+                        },
+                        user: {
+                            id: user.screen_name,
+                            name: user.name,
+                            image: user.profile_image_url_https,
+                        },
+                    };
+                });
+                console.log(result);
+                fs.writeFileSync(
+                    `${process.cwd()}/output/${fname}_x${fcount}.json`,
+                    JSON.stringify(result, null, "\t")
+                );
+            } else {
+                console.log({ error });
+            }
+        }
+    );
+})()
